21607f2b386338a99edf92b010e3249308a5f098
muhamm3dyilmaz <mylmz2101@gmail.com>
2024-12-04 11:14:28 +0300

    Student model created

---
+++ src/main/java/com/project/spring_security_app/model/Student.java
@@ -0,0 +1,16 @@
+package com.project.spring_security_app.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Student {
+
+    private int id;
+    private String name;
+    private String tech;
+
+}

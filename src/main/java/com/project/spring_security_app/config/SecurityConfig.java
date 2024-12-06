9bf916d0f27582110616a0ca98d9fd8b6b7c7c77
muhamm3dyilmaz <mylmz2101@gmail.com>
2024-12-06 14:38:27 +0300

    authentication provider added

--- src/main/java/com/project/spring_security_app/config/SecurityConfig.java
+++ src/main/java/com/project/spring_security_app/config/SecurityConfig.java
@@ -1,20 +1,38 @@
 package com.project.spring_security_app.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+// import org.springframework.security.core.userdetails.User;
+// import org.springframework.security.core.userdetails.UserDetails;
+// import org.springframework.security.core.userdetails.UserDetailsService;
+// import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Bean
+    public AuthenticationProvider authenticationProvider(){
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+
+        provider.setUserDetailsService(userDetailsService);
+        provider.setPasswordEncoder(NoOpPasswordEncoder.getInstance());
+
+        return provider;
+    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -27,22 +45,20 @@ public class SecurityConfig {
         return http.build();
     }
 
-    @Bean
-    public UserDetailsService userDetailsService() {
-        UserDetails user = User
-                .withDefaultPasswordEncoder()
-                .username("muhammed")
-                .password("123")
-                .roles("USER")
-                .build();
-
-        UserDetails admin = User
-                .withDefaultPasswordEncoder()
-                .username("yilmaz")
-                .password("123")
-                .roles("ADMIN")
-                .build();
-
-        return new InMemoryUserDetailsManager(user, admin);
-    }
+    // @Bean
+    // public UserDetailsService userDetailsService() {
+    //     UserDetails user = User
+    //             .withDefaultPasswordEncoder()
+    //             .username("muhammed")
+    //             .password("123")
+    //             .roles("USER")
+    //             .build();
+    //     UserDetails admin = User
+    //             .withDefaultPasswordEncoder()
+    //             .username("yilmaz")
+    //             .password("123")
+    //             .roles("ADMIN")
+    //             .build();
+    //     return new InMemoryUserDetailsManager(user, admin);
+    // }
 }

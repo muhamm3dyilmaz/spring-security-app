e63b08b9655531ced9e01e5229ee92560d3d3528
muhamm3dyilmaz <mylmz2101@gmail.com>
2024-12-16 16:03:14 +0300

    bcryptencoder moved on to user service

--- src/main/java/com/project/spring_security_app/controller/UserController.java
+++ src/main/java/com/project/spring_security_app/controller/UserController.java
@@ -7,7 +7,6 @@ import com.project.spring_security_app.model.User;
 import com.project.spring_security_app.service.UserService;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @RestController
@@ -15,11 +14,10 @@ public class UserController {
 
     @Autowired
     private UserService userService;
-    private BCryptPasswordEncoder encoder = new BCryptPasswordEncoder(12);
+    
 
     @PostMapping("register")
     public User register(@RequestBody User user) {
-        user.setPassword(encoder.encode(user.getPassword()));
         return userService.saveUser(user);
     }
 
--- src/main/java/com/project/spring_security_app/service/UserService.java
+++ src/main/java/com/project/spring_security_app/service/UserService.java
@@ -1,6 +1,7 @@
 package com.project.spring_security_app.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.project.spring_security_app.model.User;
@@ -11,8 +12,10 @@ public class UserService {
 
     @Autowired
     private UserRepository userRepository;
+    private BCryptPasswordEncoder encoder = new BCryptPasswordEncoder(12);
 
     public User saveUser(User user){
+        user.setPassword(encoder.encode(user.getPassword()));
         return userRepository.save(user);
     }
 

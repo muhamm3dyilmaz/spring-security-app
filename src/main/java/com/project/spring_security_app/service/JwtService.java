656900ea2a594377e995c71e215030757a36bd62
Muhammed YILMAZ <mylmz2101@gmail.com>
2025-03-02 14:08:50 +0300

    token generation done

--- src/main/java/com/project/spring_security_app/service/JwtService.java
+++ src/main/java/com/project/spring_security_app/service/JwtService.java
@@ -1,27 +1,56 @@
 package com.project.spring_security_app.service;
 
+import java.security.Key;
+import java.util.Base64;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
+
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
 
 import org.springframework.stereotype.Service;
 
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 
 @Service
 public class JwtService {
+
+    private String secretKey;
+
+    public JwtService() {
+        secretKey = generateSecretKey();
+    }
+
+    public String generateSecretKey() {
+        try {
+            KeyGenerator keyGen = KeyGenerator.getInstance("HmacSHA256");
+            SecretKey secretKey = keyGen.generateKey();
+            System.out.println("Secret Key:" + secretKey.toString());
+            return Base64.getEncoder().encodeToString(secretKey.getEncoded());
+        } catch (Exception e) {
+            throw new RuntimeException("Error generating secret key", e);
+        }
+    }
 
     public String generateToken(String username) {
         Map<String, Object> claims = new HashMap<>();
 
         return Jwts.builder()
-                .claims(claims)
-                .subject(username)
-                .issuedAt(new Date(System.currentTimeMillis()))
-                .expiration(new Date(System.currentTimeMillis() + 1000 * 60 * 3))
-                .signWith(getKey(), Jwts.SIG.HS256)
+                .setClaims(claims)
+                .setSubject(username)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 3))
+                .signWith(getKey(), SignatureAlgorithm.HS256)
                 .compact();
+    }
+
+    private Key getKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(secretKey);
+        return Keys.hmacShaKeyFor(keyBytes);
     }
 
 }

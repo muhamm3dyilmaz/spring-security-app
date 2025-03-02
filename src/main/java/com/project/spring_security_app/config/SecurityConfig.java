a9794c3ff7774032bb244cf1253b5bb73618961a
Muhammed YILMAZ <mylmz2101@gmail.com>
2025-03-02 12:58:09 +0300

    jwt register implemented

--- src/main/java/com/project/spring_security_app/config/SecurityConfig.java
+++ src/main/java/com/project/spring_security_app/config/SecurityConfig.java
@@ -5,7 +5,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -39,8 +38,6 @@ public class SecurityConfig {
                         .anyRequest()
                         .authenticated())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
-
-        http.httpBasic(Customizer.withDefaults());
 
         return http.build();
     }
--- src/main/java/com/project/spring_security_app/model/User.java
+++ src/main/java/com/project/spring_security_app/model/User.java
@@ -13,7 +13,7 @@ import lombok.Data;
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int Id;
+    private int id;
     private String username;
     private String password;
 }

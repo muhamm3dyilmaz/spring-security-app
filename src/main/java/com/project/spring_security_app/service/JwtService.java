74fe6fe2c72ea41adf561bce84c09bc8fb4c7a94
Muhammed YILMAZ <mylmz2101@gmail.com>
2025-03-02 13:35:35 +0300

    JwtService - generateToken created

---
+++ src/main/java/com/project/spring_security_app/service/JwtService.java
@@ -0,0 +1,27 @@
+package com.project.spring_security_app.service;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.stereotype.Service;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+@Service
+public class JwtService {
+
+    public String generateToken(String username) {
+        Map<String, Object> claims = new HashMap<>();
+
+        return Jwts.builder()
+                .claims(claims)
+                .subject(username)
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis() + 1000 * 60 * 3))
+                .signWith(getKey(), Jwts.SIG.HS256)
+                .compact();
+    }
+
+}

443af13cba27fcd77575cabd7934fd08fa44853a
Muhammed YILMAZ <mylmz2101@gmail.com>
2025-03-02 13:12:26 +0300

    login endpoint created

--- src/main/java/com/project/spring_security_app/controller/UserController.java
+++ src/main/java/com/project/spring_security_app/controller/UserController.java
@@ -7,6 +7,9 @@ import com.project.spring_security_app.model.User;
 import com.project.spring_security_app.service.UserService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @RestController
@@ -14,11 +17,25 @@ public class UserController {
 
     @Autowired
     private UserService userService;
-    
+    @Autowired
+    AuthenticationManager authenticationManager;
 
     @PostMapping("register")
     public User register(@RequestBody User user) {
         return userService.saveUser(user);
+    }
+
+    @PostMapping("login")
+    public String login(@RequestBody User user) {
+
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(user.getUsername(), user.getPassword()));
+
+        if (authentication.isAuthenticated()) {
+            return "Login Success!";
+        } else {
+            return "Login Failed!";
+        }
     }
 
 }
